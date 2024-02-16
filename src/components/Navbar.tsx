8f844fba6f1193317cc33ad6050674547444e9e3
haikalmohdpuat <haikal@fulkrum.net>
2024-02-16 20:55:09 +0800

    change app name and icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,9 +13,9 @@ const Navbar = async () => {
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
         {/*Logo*/}
         <Link href="/" className="flex gap-2 items-center">
-          <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6" />
+          <Icons.logo className="h-20 w-20 sm:h-20 sm:w-20" />
           <p className="hidden text-zinc-700 text-sm font-medium md:block">
-            Breadit
+            Diskusi
           </p>
         </Link>
         {/*Search Bar*/}
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -12,14 +12,14 @@ const SignIn = () => {
           <Icons.logo className="mx-auto h-6 w-6"/>
           <h1 className="text-2xl font-semibold tracking-tight">Welcome back</h1>
           <p className="text-sm max-w-xs mx-auto">
-            By continuing, you are setting up a Breadit account and agree to our User Agreement and Privacy Policy
+            By continuing, you are setting up a Diskusi account and agree to our User Agreement and Privacy Policy
           </p>
 
           {/*sign in form*/}
           <UserAuthForm/>
 
           <p className="px-8 text-center text-sm text-zinc-700">
-            New to Breadit?{' '}
+            New to Diskusi?{' '}
             <Link href='/sign-up' className="hover:text-zinc-800 text-sm underline underline-offset-4">Sign Up</Link>
           </p>
       </div>
